682e62028eda7d06f5cee9fd016df77bc5b2d8f6
MarkA <marklacap14.ml@gmail.com>
2023-07-09 14:05:30 +0800

    Spawn 16x16 squares

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const container = document.querySelector('.container');
 const buttonSpawn = document.querySelector('.spawn');
 
+let clientWidth = container.clientWidth;
+let clientHeight = container.clientHeight;
+console.log(clientHeight);
+console.log(clientWidth);
+
 function spawnBoxes(){
     let squareLot;
-    let numOfBox = () => {
-        return 
-    };
+    let numOfBox = 16**2;
     for(let i = 0; i < numOfBox; i++){
         squareLot = document.createElement('div');
         container.append(squareLot);
         squareLot.setAttribute('class','square');
+        squareLot.style.width = calculateSquareSize();
+        squareLot.style.height = calculateSquareSize();    
     }
+
 }
 
-buttonSpawn.addEventListener("click", spawnBoxes)
+function calculateSquareSize() {
+    let size = clientWidth/16;
+    return size.toString() + "px";
+}
+
+buttonSpawn.addEventListener("click", spawnBoxes);
