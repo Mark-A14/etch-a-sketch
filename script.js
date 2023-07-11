e579844574922e0c306f7f006851584ee95b2420
MarkA <marklacap14.ml@gmail.com>
2023-07-11 18:11:44 +0800

    Replaced creating items from flex to grid items

--- script.js
+++ script.js
@@ -5,30 +5,34 @@ let sliderOutput = document.querySelector('.rangeOutput');
 sliderOutput.innerText = slider.value;
 slider.oninput = function () {
     sliderOutput.innerText = this.value.toString() + 'x' + this.value.toString();
+    
 }
 
 function spawnBoxes(){
     if(container.hasChildNodes()){
         return;
     }
-    let squareSize = 500/slider.value - 2;
+    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
     for(let i = 0; i < slider.value**2; i++){
         let squareDiv = document.createElement('div');
         container.append(squareDiv);
         squareDiv.classList.add('square');
-        squareDiv.style.width = squareSize.toString() + 'px';
-        squareDiv.style.height = squareSize.toString() + 'px';
+        squareDiv.addEventListener('mouseenter', applyColor);
     }
 }
 
 btnSpawn.addEventListener('click', spawnBoxes);
 container.addEventListener('mousemove', mouseEvent);
 
-function mouseEvent(e){
-    let boxes = document.querySelectorAll('.square');
-    boxes.forEach(n => {
-        n.addEventListener('mouseenter', function () {
-            this.classList.add('hovered');
-        })
-    });
+function applyColor(event){
+
+}
+
+function clearGrid(event){
+    
+}
+
+function changeMode(event){
+    
 }
