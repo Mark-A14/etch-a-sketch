0bdc9e63fd98bf9ded571405bfe8a1b47094b499
MarkA <marklacap14.ml@gmail.com>
2023-07-13 18:43:28 +0800

    Added functionality for progressive button

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
-let container = document.querySelector('.container');
-let btnSpawn = document.querySelector('.spawn');
-let slider = document.querySelector('#boxes');
-let sliderOutput = document.querySelector('.rangeOutput');
-sliderOutput.innerText = slider.value;
+const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = 16;
+let mode = DEFAULT_MODE;
+let container = document.querySelector(".container");
+let buttonRainbow = document.querySelector("#rainbow");
+let buttonClear = document.querySelector("#clear");
+let buttonProgressive = document.querySelector("#progressive");
+let colorPicked = document.querySelector("#colorPicker");
+let slider = document.querySelector("#boxes");
+let sliderOutput = document.querySelector(".rangeOutput");
+
+sliderOutput.innerText =
+	slider.value.toString() + "x" + slider.value.toString();
 slider.oninput = function () {
-    sliderOutput.innerText = this.value.toString() + 'x' + this.value.toString();
-    
+	sliderOutput.innerText =
+		this.value.toString() + "x" + this.value.toString();
+	removeGrid();
+	createGrids(this.value);
+};
+
+function changeMode(event) {
+	mode = this.value;
+	console.log(mode);
 }
 
-function spawnBoxes(){
-    if(container.hasChildNodes()){
-        return;
-    }
-    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
-    for(let i = 0; i < slider.value**2; i++){
-        let squareDiv = document.createElement('div');
-        container.append(squareDiv);
-        squareDiv.classList.add('square');
-        squareDiv.addEventListener('mouseenter', applyColor);
-    }
+function createGrids(size) {
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	for (let i = 0; i < size ** 2; i++) {
+		let squareDiv = document.createElement("div");
+		container.append(squareDiv);
+		squareDiv.classList.add("square");
+		squareDiv.addEventListener("mouseenter", applyColor);
+	}
 }
 
-btnSpawn.addEventListener('click', spawnBoxes);
-container.addEventListener('mousemove', mouseEvent);
+function applyColor() {
+	if (mode === DEFAULT_MODE) {
+		this.style.backgroundColor = colorPicked.value;
+	}
+	if (mode === "rainbow") {
+		let randomRed = Math.floor(Math.random() * 256);
+		let randomBlue = Math.floor(Math.random() * 256);
+		let randomGreen = Math.floor(Math.random() * 256);
+		this.style.backgroundColor = `rgb(${randomRed},${randomGreen},${randomBlue})`;
+	}
+	if (mode === "prog") {
+		if (this.style.backgroundColor == "") {
+			this.style.backgroundColor = colorPicked.value;
+			this.style.opacity = "0.1";
+		}
 
-function applyColor(event){
-
+		if (this.style.opacity != "1") {
+			let opacity = parseFloat(this.style.opacity, 10);
+			opacity += 0.1;
+			this.style.opacity = opacity.toString();
+		}
+	}
 }
 
-function clearGrid(event){
-    
+function clearGrid() {
+	// let grids = document.querySelectorAll(".square");
+	// grids.forEach((grid) => {
+	// 	grid.style.backgroundColor = "white";
+	// });
+	removeGrid();
+	createGrids(slider.value);
 }
 
-function changeMode(event){
-    
+function removeGrid() {
+	container.innerHTML = "";
 }
+
+buttonRainbow.addEventListener("click", changeMode);
+buttonClear.addEventListener("click", clearGrid);
+buttonProgressive.addEventListener("click", changeMode);
+colorPicked.addEventListener("click", function () {
+	mode = DEFAULT_MODE;
+});
+createGrids(DEFAULT_SIZE);
